5eeaaf7ead18c23526734331e3cbc9563b5acc5f
project 1 <zegorbelikov@gmail.com>
2025-05-08 19:57:37 +1000

    git commit

--- src/Calendar.java
+++ src/Calendar.java
@@ -7,19 +7,20 @@ public class Calendar {
         System.out.println("Input number of the month");
         int month = sc.nextInt();
         switch(month) {
-            case 1: return "January";
-            case 2: return "February";
-            case 3: return "March";
-            case 4: return "April";
-            case 5: return "May";
-            case 6: return "June";
-            case 7: return "July";
-            case 8: return "August";
-            case 9: return "September";
-            case 10: return "October";
-            case 11: return "November";
-            case 12: return "December";
-            default: return "Invalid month";
+            case 1: return"-----------January---------";
+            case 2: return"----------February---------";
+            case 3: return"-----------March-----------";
+            case 4: return"-----------April-----------";
+            case 5: return"------------May------------";
+            case 6: return"------------June-----------";
+            case 7: return"------------July-----------";
+            case 8: return"-----------August----------";
+            case 9: return"---------September---------";
+            case 10: return"----------October----------";
+            case 11: return"----------November---------";
+            case 12: return"----------December---------";
+            default: System.out.println("Invalid month"); System.exit(0);
+            return "Invalid month";
         }
     }
     public static int getYear() {
@@ -29,6 +30,14 @@ public class Calendar {
     public static void print(){
         System.out.println(getYear());
         System.out.println(getMonth());
-        System.out.println("Monday" + "Tuesday" + "Wednesday" + "Thursday");
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
     }
+    public static String findLeapYear(int year){
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return "Leap Year";
+        } else {
+            return "Not a Leap Year";
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Homework.reverse(1256));
-            }
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+    }
         }
