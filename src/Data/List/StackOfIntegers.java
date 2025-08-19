006af7dd617c52488745a66cdda59486d1370ac4
project 1 <zegorbelikov@gmail.com>
2025-08-19 17:37:19 +1000

    git commit

--- src/Data/List/StackOfIntegers.java
+++ src/Data/List/StackOfIntegers.java
@@ -5,39 +5,85 @@ public class StackOfIntegers {
     private int top;
     private int capacity;
 
-    public StackOfIntegers(int size){
-
+    public StackOfIntegers(int size) {
+        capacity = size;
+        elements = new int[capacity];
     }
 
-    public void push(){
-        //add to the top
+    public void push(int value) {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == null) {
+                elements[i] = value;
+            }
+        }
     }
-    public int pop(){
-        //return top element and remove from stack
-    }
-    public int peek(){
-        //return top element
-    }
-    public boolean isEmpty(){
 
+    public int pop() {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == null) {
+                i--;
+                top = elements[i];
+                elements[i] = null;
+            }
+        }
+        return top;
     }
-    public boolean isFull(){
 
+    public int peek() {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == null) {
+                i--;
+                top = elements[i];
+            }
+        }
+        return top;
     }
-    public int size(){
-        //return size
+
+    public boolean isEmpty() {
+        if (elements[0] == null) {
+        }
+        return true;
     }
-    public void clear(){
-        //clear massive
+
+    public boolean isFull() {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == null) {
+                return false;
+            }
+        }
+        return true;
     }
-    public int search(int value){
-        //look for value from the top
+
+    public int size() {
+        return elements.length;
     }
-    public boolean contains(int value){
-        //check if massive has the value
+
+    public void clear() {
+        elements = null;
     }
-    public void print(){
-        //prints out the stack
+
+    public int search(int value) {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == value) {
+                return elements[i];
+            }
+        }
+        return 0;
+    }
+
+    public boolean contains(int value) {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void print() {
+        for (int i = 0; i < elements.length; i++) {
+            System.out.print(elements[i] + " ");
+        }
     }
 }
 //test the class
---
+++ src/Test/LoanTest.java
@@ -0,0 +1,5 @@
+package Test;
+
+public class LoanTest {
+    Loan loan = new Loan();
+}
